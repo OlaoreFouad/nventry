479a59255dba40d75aff15e631f980fb7c9ded6f
Olaore Fouad <olaorefouad565@gmail.com>
2021-05-02 17:18:49 +0400

    feature: worked on the design for the main screen (business and profile), also added the design for a single business item, on bro!

--- app/src/main/java/dev/olaore/nventry/MainActivity.kt
+++ app/src/main/java/dev/olaore/nventry/MainActivity.kt
@@ -2,10 +2,27 @@ package dev.olaore.nventry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.main_bottom_nav)
+        setupNavigation()
+
     }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+
+    }
+
 }
